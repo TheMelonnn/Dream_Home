0ce401c17bbfab4e08c3feb9fc642217f3a986e3
TheMelonnn <zidanrifardi125@gmail.com>
2025-10-21 23:20:46 +0800

    add crawling script + create dashboard view

---
+++ createdb.py
@@ -0,0 +1,12 @@
+import sqlite3
+
+conn = sqlite3.connect('database.db')
+c = conn.cursor()
+c.execute('''
+    CREATE TABLE IF NOT EXISTS rooms (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT NOT NULL
+    )
+''')
+conn.commit()
+conn.close()
